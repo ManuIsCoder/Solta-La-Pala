44893c9c329748a9235887a8e703f37d3bc81e9c
akcjsbfifb <martinmoloeznik@gmail.com>
2026-09-25 14:24:47 -0300

    borre proyecto demo y primer nivel terminado diseño escena

--- NpcDetectionDemo/Assets/Scripts/Zona.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Zona : MonoBehaviour
-{
-    public string nombre;
-    public List<Npc> npcsPresentes = new List<Npc>();
-}
--- NpcDetectionDemo/README.md
+++
@@ -1,5 +0,0 @@
-# stealth-npc-detection
-
-Proyecto Unity: `NpcDetectionDemo`.
-
-Scripts: `Npc.cs`, `CampoDeVision.cs` (basado en Guard.cs de Unity_Stealth_Game).
